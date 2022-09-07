744e040ab06acf73ab8b09f58b81b8d68063e77c
vismund <nickhughes412@gmail.com>
2022-09-07 21:29:46 +0300

    Add asynchronous task

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
 from orders.models import OrderItem
+from .tasks import order_created
 
 
 def order_create(request):
@@ -16,6 +17,8 @@ def order_create(request):
                     order=order, product=item['product'], price=item['price'], quantity=item['quantity']
                 )
             cart.clear()
+            # Run asynchronous task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
         else:
             form = OrderCreateForm()
